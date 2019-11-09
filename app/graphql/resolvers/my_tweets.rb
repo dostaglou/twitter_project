1014911959d06618444c78e33ed66d864604fcef
Dostaglou <dostaglou@gmail.com>
2019-11-10 00:56:10 +0900

    added pagination to 'my tweets'

--- app/graphql/resolvers/my_tweets.rb
+++ app/graphql/resolvers/my_tweets.rb
@@ -1,9 +1,10 @@
 module Resolvers
     class MyTweets < Resolvers::Base
+        argument :pagination, Types::Pagination, required: false
         type [Types::TweetType], null: false
-        def resolve
+        def resolve(pagination:)
             return GraphQL::ExecutionError.new("Must be signed in to access this feature") unless context[:current_user]
-            context[:current_user]&.tweets
+            context[:current_user]&.tweets&.offset(pagination[0])&.limit(pagination[1])
         end
     end
 end
--- app/graphql/types/pagination.rb
+++ app/graphql/types/pagination.rb
@@ -7,7 +7,7 @@ module Types
         
         def prepare 
             return GraphQL::ExecutionError.new("limit must be between 1 and 50") unless (1..50).include?(limit)
-            return GraphQL::ExecutionError.new("offset must be between 1 and 500") unless (1..500).include?(offset)
+            return GraphQL::ExecutionError.new("offset must be between 0 and 500") unless (0..500).include?(offset)
             [offset, limit]
         end
     end
