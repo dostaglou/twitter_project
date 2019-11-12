3cdb91289b66fd3648a36237f3ce1bd4417b9a55
Dostaglou <dostaglou@gmail.com>
2019-11-12 11:03:15 +0900

    msg for unlike done

--- app/graphql/mutations/unlike.rb
+++ app/graphql/mutations/unlike.rb
@@ -2,13 +2,15 @@ module Mutations
     class Unlike < BaseMutation
         argument :tweet_id, ID, required: true 
 
-        type Types::TweetType
+        field :original_tweet, Types::TweetType, null: true
+        field :message, String, null: true
 
         def resolve(tweet_id:)
             self.me?
             t = Tweet.find(tweet_id)
             Like.find_by(user_id: me.id, tweet_id: t.id)&.destroy
-            t
+            {original_tweet: t, message: "You unliked the tweet" }
+
         rescue ActiveRecord::RecordInvalid => e
             GraphQL::ExecutionError.new("Either tweet doesn't exist or you don't like it")
         end
